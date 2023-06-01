9c8ca710920c5640967bd126d9b0f1eb751c1d26
Khanh Truong <ckhanh595@gmail.com>
2023-06-01 10:53:39 +0700

    Initial commit

---
+++ src/main/java/com/course/elastic/api/server/DefaultRestApi.java
@@ -0,0 +1,24 @@
+package com.course.elastic.api.server;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+
+@RestController
+@RequestMapping(value = "/api")
+public class DefaultRestApi {
+
+    @GetMapping(value = "/welcome")
+    public String welcome() {
+        System.out.println(StringUtils.join("Hello", " this is", " Spring boot", " REST API"));
+        return "Welcome to spring boot";
+    }
+
+    @GetMapping(value = "/time")
+    public String time() {
+        return LocalTime.now().toString();
+    }
+}
---
+++ src/test/java/com/course/elastic/ElasticsearchSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.elastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
